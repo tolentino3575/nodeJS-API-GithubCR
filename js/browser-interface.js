7cc6d703884f4d2d24b6d24c4d50fe3bba783b08
erik tolentino <tolentino3575@gmail.com>
2016-03-18 12:27:39 -0700

    Add avatar image

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,9 +1,13 @@
 var getRepos = require('./../js/github.js').getRepos;
+var getUserInfo = require('./../js/github.js').getUserInfo;
 
 $(document).ready (function(){
   $('#github').submit(function(event){
     event.preventDefault();
+    $('#repoInfo').show();
+    $('#userInfo').show();
     var userName = $('#userName').val();
     getRepos(userName);
+    getUserInfo(userName);
   });
 });
--- js/github.js
+++ js/github.js
@@ -1,9 +1,9 @@
 var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(userName){
-  $('#repoInfo').show();
+
   $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
-    console.log(response);
+    // console.log(response);
     for(var i = 0; i < response.length; i++) {
       var name = response[i].name;
       var desc = response[i].description;
@@ -11,8 +11,36 @@ exports.getRepos = function(userName){
       if(response[i].description === ""){
         var desc = "No description provided for this repository.";
       }
-      $('#repoInfo').append("<li>" + "Project name: " + name + "</li>" + "<li>" + "Description: " + desc + "</li>" + "<li>" + "Found under Github user: " + userName + "</li>" + "<li>" + "Created on: " + created + "</li>" + "<br>");
+      $('#repoInfo').append("<li>" + "Project name: " + name + "</li>" +
+                            "<li>" + "Description: " + desc + "</li>" +
+                            "<li>" + "Found under Github user: " + userName + "</li>" +
+                            "<li>" + "Created on: " + created + "</li>" +
+                            "<br>");
     };
+  }).fail(function(error){
+    // console.log(error.responseJSON.message);
+  });
+};
+
+exports.getUserInfo = function(userName){
+  $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    var fullName = response.name;
+    if(response.name === null){
+      var fullName = "Not provided";
+    }
+    var followers = response.followers;
+    var following = response.following;
+    var email = response.email;
+    if(response.email === null){
+      var email = "Not provided"
+    }
+    var avatar = response.avatar_url;
+    $('#userInfo').append("<li>" + "The name of the owner of this Github account is: " + fullName + "</li>" +
+                          "<li>" + "They are following " + following + " other Github users." + "</li>" +
+                          "<li>" + "They have " + followers + " other Github users following them." + "</li>" +
+                          "<li>" + "If you wish to contact " + userName + ", their email is: " + email + "</li>" +
+                          "<li>" + "This is what " + userName + " looks like: " + "<img src=" + avatar + ">" + "</li>");
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
