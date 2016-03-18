24af9381a7cda73c0990ff682835192cde869d3e
erik tolentino <tolentino3575@gmail.com>
2016-03-18 11:46:17 -0700

    Add moment.js to view date repo created, edit some beautiful styling

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -3,7 +3,6 @@ var getRepos = require('./../js/github.js').getRepos;
 $(document).ready (function(){
   $('#github').submit(function(event){
     event.preventDefault();
-    $('#repoInfo').show();
     var userName = $('#userName').val();
     getRepos(userName);
   });
--- js/github.js
+++ js/github.js
@@ -1,12 +1,17 @@
 var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(userName){
+  $('#repoInfo').show();
   $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
     for(var i = 0; i < response.length; i++) {
       var name = response[i].name;
       var desc = response[i].description;
-      $('#repoInfo').append("<li>" + "Name: " + name + "</li>" + "<li>" + "Description: " + desc + "</li>" + "<li>" + "Found under user: " + userName + "</li>");
+      var created = moment(response[i].created_at).format('MMMM Do YYYY, h:mm:ss:a');
+      if(response[i].description === ""){
+        var desc = "No description provided for this repository.";
+      }
+      $('#repoInfo').append("<li>" + "Project name: " + name + "</li>" + "<li>" + "Description: " + desc + "</li>" + "<li>" + "Found under Github user: " + userName + "</li>" + "<li>" + "Created on: " + created + "</li>" + "<br>");
     };
   }).fail(function(error){
     console.log(error.responseJSON.message);
