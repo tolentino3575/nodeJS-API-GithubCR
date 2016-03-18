494ed1fb3d5fe3999e4a3a2c44ed4fa79d81e3ed
erik tolentino <tolentino3575@gmail.com>
2016-03-18 10:49:03 -0700

    Display repositories from search

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -3,6 +3,7 @@ var getRepos = require('./../js/github.js').getRepos;
 $(document).ready (function(){
   $('#github').submit(function(event){
     event.preventDefault();
+    $('#repoInfo').show();
     var userName = $('#userName').val();
     getRepos(userName);
   });
--- js/github.js
+++ js/github.js
@@ -3,6 +3,11 @@ var apiKey = require('./../.env').apiKey;
 exports.getRepos = function(userName){
   $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
+    for(var i = 0; i < response.length; i++) {
+      var name = response[i].name;
+      var desc = response[i].description;
+      $('#repoInfo').append("<li>" + "Name: " + name + " with the description: " + desc + " has been found under user: " + userName + "</li>");
+    };
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
